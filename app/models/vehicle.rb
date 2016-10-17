6588018e490ec96bfee05b03c167f31b2a344360
Josh Graber <josh.graber.85@gmail.com>
2016-10-17 08:56:49 -0400

    Update vehicle fields to display model properties
    Model properties are not implemented yet.

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -7,4 +7,24 @@ class Vehicle < ApplicationRecord
   def needs_maintenance?
     true
   end
+
+  def lifetime_mileage
+    base_mileage
+  end
+
+  def recent_mileage(months)
+    months
+  end
+
+  def miles_since_maintenance
+    0
+  end
+
+  def lifetime_mpg
+    0
+  end
+
+  def recent_mpg(months)
+    months
+  end
 end
