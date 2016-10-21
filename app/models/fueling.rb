be589e1709a939f126e5ed29ef40c306c30feb93
Josh Graber <josh.graber.85@gmail.com>
2016-10-21 08:56:51 -0400

    Add model property stubs to support view

--- app/models/fueling.rb
+++ app/models/fueling.rb
@@ -1,3 +1,17 @@
 class Fueling < ApplicationRecord
   belongs_to :vehicle
+
+  validates :date, presence: true
+
+  def mpg
+    0
+  end
+
+  def above_average_mpg?
+    false
+  end
+
+  def below_average_mpg?
+    true
+  end
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -27,4 +27,21 @@ class Vehicle < ApplicationRecord
   def recent_mpg(months)
     months
   end
+
+  def needs_maintenance?
+    false
+  end
+
+  def last_maintenance
+    maintenance.last
+  end
+
+  def line_items
+    fueling_line_items = fuelings.map { |f| LineItem.from_fueling f }
+    maintenance_line_items = maintenance.map { |m| LineItem.from_maintenance m }
+    all_items = (fueling_line_items + maintenance_line_items)
+    all_items.sort do |first, second|
+      second.date <=> first.date
+    end
+  end
 end
