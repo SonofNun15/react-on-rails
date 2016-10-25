7fb4d2294c7e9a2d5885a6ecbba86f6181517685
Josh Graber <josh.graber.85@gmail.com>
2016-10-24 21:40:12 -0400

    If vehicle logic, handle vehicles with no maintenance or fueling entries

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -7,7 +7,8 @@ class Vehicle < ApplicationRecord
   has_many :maintenance
 
   def needs_maintenance?
-    last_maintenance.date < 6.months.ago ||
+    last_maintenance.nil? ||
+      last_maintenance.date < 6.months.ago ||
       miles_since_maintenance > 4000
   end
 
@@ -21,16 +22,20 @@ class Vehicle < ApplicationRecord
   end
 
   def miles_since_maintenance
-    fuelings_since_maintenance = fuelings.where 'date > ?', last_maintenance.date
-    fuelings_since_maintenance.sum { |f| f.miles }
+    if last_maintenance.present?
+      fuelings_since_maintenance = fuelings.where 'date > ?', last_maintenance.date
+      fuelings_since_maintenance.sum { |f| f.miles }
+    end
   end
 
   def lifetime_mpg
-    fuelings.all.to_a.average { |f| f.mpg }
+    fuelings.all.to_a.average { |f| f.mpg } if fuelings.any?
   end
 
   def recent_mpg(num_months)
-    recent_fuelings(num_months).to_a.average { |f| f.mpg }
+    if fuelings.any?
+      recent_fuelings(num_months).to_a.average { |f| f.mpg }
+    end
   end
 
   def last_maintenance
