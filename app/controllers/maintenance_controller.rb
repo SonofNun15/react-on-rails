d97e30f6c075be61d2cac70dca38f81955b8fdc5
Josh Graber <josh.graber.85@gmail.com>
2016-10-24 21:50:48 -0400

    Implement edit and delete for maintenance

--- app/controllers/maintenance_controller.rb
+++ app/controllers/maintenance_controller.rb
@@ -1,7 +1,16 @@
-class MaintenanceController < AuthenticatedController
+class MaintenanceController < AuthorizeVehicleController
+  before_action :fetch_maintenance, except: [:new, :create]
+
+  def initialize
+    super(:vehicle_id)
+  end
+
   def new
     @maintenance = Maintenance.new
     @maintenance.date = Date.today
+    @action_text = 'Add'
+    @action = vehicle_maintenance_index_path
+    render 'edit'
   end
 
   def create
@@ -11,7 +20,28 @@ class MaintenanceController < AuthenticatedController
     redirect_to vehicle_path params[:vehicle_id]
   end
 
+  def edit
+    @action_text = 'Save'
+    @action = vehicle_maintenance_path params[:vehicle_id], params[:id]
+  end
+
+  def update
+    @maintenance.update maintenance_params
+    flash[:notice] = 'Maintenance updated'
+    redirect_to vehicle_path params[:vehicle_id]
+  end
+
+  def destroy
+    @maintenance.destroy
+    flash[:notice] = 'Maintenance removed'
+    redirect_to vehicle_path params[:vehicle_id]
+  end
+
   private
+
+  def fetch_maintenance
+    @maintenance = Maintenance.find params[:id]
+  end
 
   def maintenance_params
     params.require(:maintenance).permit(:date, :description, :mechanic, :cost)
