a53e1fb1b34544a6e2573031c7b171232ed6e3df
Josh Graber <josh.graber.85@gmail.com>
2016-11-04 08:58:05 -0400

    Implement maintenance data items

--- app/assets/javascripts/components/vehicle/table.js.jsx
+++ app/assets/javascripts/components/vehicle/table.js.jsx
@@ -1,8 +1,23 @@
 var VehicleDataTable = React.createClass({
+  propTypes: {
+    vehicleId: React.PropTypes.number,
+    lineItems: React.PropTypes.array,
+  },
+
   render: function() {
     return (
       <div className="data-table">
-        Data soon...
+        {
+          this.props.lineItems.map(function(item) {
+            if (item.type == 'fueling') {
+              return <FuelingEntry key={item.key} vehicleId={this.props.vehicleId}
+                                   fueling={item} />
+            } else if (item.type == 'maintenance') {
+              return <MaintenanceEntry key={item.key} vehicleId={this.props.vehicleId}
+                                       maintenance={item} />
+            }
+          }.bind(this))
+        }
       </div>
     );
   },
