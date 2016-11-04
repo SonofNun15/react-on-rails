69c0595b0ed3e2f5f18822631296198cf3279d01
Josh Graber <josh.graber.85@gmail.com>
2016-11-04 09:25:20 -0400

    Add buttons display an editor inline now

---
+++ app/assets/javascripts/components/vehicle/maintenance.js.jsx
@@ -0,0 +1,24 @@
+var MaintenanceEditor = React.createClass({
+  propTypes: {
+    close: React.PropTypes.func,
+  },
+
+  close: function() {
+    this.props.close();
+  },
+
+  render: function() {
+    return (
+      <div>
+        Maintenance
+        <div className="pull-right buttons">
+          <button type="submit" className="btn btn-danger"
+                  onClick={this.close}>
+            <i className="fa fa-times"></i> Close
+          </button>
+        </div>
+        <div className="clearfix"></div>
+      </div>
+    );
+  },
+});
