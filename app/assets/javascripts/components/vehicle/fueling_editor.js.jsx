231bd578fc189fa63baad2e67a303eae50e55fde
Josh Graber <josh.graber.85@gmail.com>
2016-11-06 13:42:05 -0500

    Add components for dynamically generating fuelings and maintenance

--- app/assets/javascripts/components/vehicle/fueling_editor.js.jsx
+++ app/assets/javascripts/components/vehicle/fueling_editor.js.jsx
@@ -9,12 +9,30 @@ var FuelingEditor = React.createClass({
 
   render: function() {
     return (
-      <div>
-        Fueling
+      <div className="form-inline">
+        <div className="form-group">
+          <input className="form-control" placeholder="Gas"
+                 type="number" />
+        </div>
+        <div className="form-group">
+          <input className="form-control" placeholder="Miles"
+                 type="number" />
+        </div>
+        <div className="form-group">
+          <input className="form-control" placeholder="Cost"
+                 type="number" />
+        </div>
+        <div className="form-group">
+          <input className="form-control" placeholder="Date"
+                 type="date" />
+        </div>
         <div className="pull-right buttons">
-          <button type="submit" className="btn btn-danger"
+          <button type="button" className="btn btn-danger"
                   onClick={this.close}>
             <i className="fa fa-times"></i> Close
+          </button>
+          <button type="button" className="btn btn-success">
+            <i className="fa fa-plus"></i> Add
           </button>
         </div>
         <div className="clearfix"></div>
