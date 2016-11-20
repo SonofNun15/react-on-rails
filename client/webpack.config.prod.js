60fbc538f845f77b5536347ef24c739365f47e55
Josh Graber <josh.graber.85@gmail.com>
2016-11-20 11:18:43 -0500

    Remove CSS stuff; not using css at this time

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -1,7 +1,6 @@
 import path from 'path'
 import webpack from 'webpack'
 import ManifestPlugin from 'webpack-manifest-plugin'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 export default {
   debug: false,
@@ -19,9 +18,6 @@ export default {
   plugins: [
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
 
-    // Generate a css file with cache busting
-    new ExtractTextPlugin('[name].[contenthash].css'),
-
     // Generate hash of bundles for cache busting
     new ManifestPlugin(),
 
@@ -34,7 +30,6 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap') }
     ]
   }
 }
