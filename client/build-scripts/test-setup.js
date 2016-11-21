045b1d5019042cc36c9bd28f6c0eeb997d559fac
Josh Graber <josh.graber.85@gmail.com>
2016-11-21 07:54:51 -0500

    Add configuration for JSDom so we can test react components

--- client/build-scripts/test-setup.js
+++ client/build-scripts/test-setup.js
@@ -2,10 +2,31 @@
 
 /* eslint-disable */
 
+process.env.NODE_ENV = 'test';
+
 // Register babel to transpile before our tests run
 require('babel-register')();
 
 // Disable webpack features that the test framework does not understand
 require.extensions['.css'] = function() {};
+
+// Configure JSDOM and set global varibles
+//  to simulate a browser environment for tests.
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach(function (property) {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js',
+};
 
 /* eslint-enable */
