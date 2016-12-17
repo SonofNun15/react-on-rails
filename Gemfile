38d6343bfddea9ecd6b3ddf4326af6cdc2550099
Josh Graber <josh.graber.85@gmail.com>
2016-12-17 10:55:31 -0500

    Enable CORS requests

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 gem 'bcrypt'
+gem 'rack-cors'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module Mileage
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: :any
+      end
+    end
   end
 end
