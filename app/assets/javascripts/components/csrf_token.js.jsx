1ca2492747fe5fcc0f17b6e0d5741eb012cba453
Josh Graber <josh.graber.85@gmail.com>
2017-01-07 10:14:39 -0500

    Remove reference to jquery and replace with vanilla javascript

--- app/assets/javascripts/components/csrf_token.js.jsx
+++ app/assets/javascripts/components/csrf_token.js.jsx
@@ -1,7 +1,13 @@
+function extractCSRFToken() {
+  var metaTag = document.querySelector('meta[name=csrf-token]');
+  return metaTag.content;
+}
+
 var CSRFToken = React.createClass({
-  csrfToken: $('meta[name=csrf-token]').attr('content'),
+  csrfToken: extractCSRFToken(),
 
   render: function() {
     return <input type='hidden' name='authenticity_token' value={this.csrfToken} />
   },
 });
+
